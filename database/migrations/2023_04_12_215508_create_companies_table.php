56092e82675f04287f61d118b13b116101be9943
Gabriel Lando <glando.py@gmail.com>
2023-04-13 00:58:00 +0300

    Add Controller, Model and Migration for Companies

---
+++ database/migrations/2023_04_12_215508_create_companies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('symbol')->unique();
+            $table->string('security_name');
+            $table->string('market_category');
+            $table->string('financial_status');
+            $table->string('test_issue');
+            $table->decimal('round_lot_size');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};

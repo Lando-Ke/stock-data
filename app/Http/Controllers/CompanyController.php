87d437bde2ffafba33a692be8d41e3433911382a
Gabriel Lando <glando.py@gmail.com>
2023-04-13 09:49:35 +0300

    No need for request

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -4,11 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Company;
 use App\Contracts\StockServiceInterface;
-use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    public function show(Request $request, StockServiceInterface $stockService, string $symbol, string $startDate, string $endDate)
+    public function show(StockServiceInterface $stockService, string $symbol, string $startDate, string $endDate)
     {
         $companyName = Company::where('symbol', $symbol)->first()->name;
         $historicalData = $stockService->getHistoricalData($symbol);
